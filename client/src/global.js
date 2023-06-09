9ddbc108051fdcf3997b63ee810f930b97835031
jieunkxx <jieun.k@hotmail.com>
2023-06-09 01:09:50 -0700

    refactor: format

--- client/src/global.js
+++ client/src/global.js
@@ -2,34 +2,34 @@ import { createGlobalStyle } from 'styled-components';
 import reset from 'styled-reset';
 
 const GlobalStyle = createGlobalStyle`
-${reset}
-	
-* {
-  box-sizing: border-box;
-}
+  ${reset}
+    
+  * {
+    box-sizing: border-box;
+  }
 
-body {
-  height: 100vh;
-  font-family: 'AirbnbCereal_W_Bk', 'Montserrat', sans-serif, Courier, monospace;
-  margin: 0;
-  padding: 0;
-  background-color: rgba(249, 247, 247, 0.872);
+  body {
+    height: 100vh;
+    font-family: 'AirbnbCereal_W_Bk', 'Montserrat', sans-serif, Courier, monospace;
+    margin: 0;
+    padding: 0;
+    background-color: rgba(249, 247, 247, 0.872);
 
-}
+  }
 
-img {
-  max-width: 100%;
-  max-height: 100%;
-}
+  img {
+    max-width: 100%;
+    max-height: 100%;
+  }
 
-a {
-  color: inherit;
-  text-decoration: inherit;
-}
+  a {
+    color: inherit;
+    text-decoration: inherit;
+  }
 
-div {
-  box-sizing: border-box;
-}
+  div {
+    box-sizing: border-box;
+  }
 	
 `;
 
