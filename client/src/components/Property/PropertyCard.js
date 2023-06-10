52eaa93d420971befaf812a1dce63fe44431577f
Chao Zhao <czha@student.ubc.ca>
2023-06-09 20:58:03 -0400

    feat: add Property Grid and Property Cards with sample data

---
+++ client/src/components/Property/PropertyCard.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import './PropertyCard.css';
+
+function PropertyCard({ property }) {
+  return (
+    <div className="property-card">
+      <img src={property.image} alt="Property" className="property-image" />
+      <div className="property-info">
+        <div className="property-price">${property.price}</div>
+        <div className="property-location">
+          {property.city}, {property.neighborhood}
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default PropertyCard;
--- client/src/pages/Home/Home.js
+++ client/src/pages/Home/Home.js
@@ -1,13 +1,39 @@
 import React, { useEffect } from 'react';
 import { useLocation, useNavigate } from 'react-router-dom';
 import styled from 'styled-components';
-import DisplayCard from '../../components/Property/DisplayCard';
+import PropertyGrid from '../../components/Property/PropertyGrid';
+import demoHouseImage from '../../assets/images/demoHouse.jpg';
 
 function Home() {
   const navigate = useNavigate();
   const location = useLocation();
+  const properties = [
+    {
+      image: demoHouseImage,
+      price: '1,000,000',
+      city: 'Vancouver',
+      neighborhood: 'Downtown',
+    },
+    {
+      image: demoHouseImage,
+      price: '2,000,000',
+      city: 'Vancouver',
+      neighborhood: 'West End',
+    },
+    {
+      image: demoHouseImage,
+      price: '2,000,000',
+      city: 'Vancouver',
+      neighborhood: 'West End',
+    },
+    // More properties...
+  ];
 
-  return <DisplayCard />;
+  return (
+    <Main>
+      <PropertyGrid properties={properties} />
+    </Main>
+  );
 }
 
 const Main = styled.div`
