79c4a218e3beb48f150da4f5b6c2fb3fee48647c
Chohyeon kim <chohyeonkim94@gmail.com>
2023-06-11 18:40:52 -0700

    refactor: delete input from Nave and move to SearchOption

--- client/src/components/Header/Nav.js
+++ client/src/components/Header/Nav.js
@@ -1,13 +1,10 @@
 import React, { useState } from 'react';
-import Input from './Input';
 import Menu from './Menu';
 
 function Nav() {
-  const [searchToggle, setSearchToggle] = useState(true);
   return (
     <div>
-      <Menu setSearchToggle={setSearchToggle} />
-      {searchToggle && <Input />}
+      <Menu />
     </div>
   );
 }
