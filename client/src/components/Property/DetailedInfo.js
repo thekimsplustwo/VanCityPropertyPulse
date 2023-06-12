c6085977fbc98cb5bba55b468a75661b74959018
Chris Su <chris019@student.ubc.ca>
2023-06-12 12:19:25 -0700

    style: adjust the layout

--- client/src/components/Property/DetailedInfo.js
+++ client/src/components/Property/DetailedInfo.js
@@ -6,6 +6,9 @@ import Button from '@mui/material/Button';
 import { Info } from '@mui/icons-material';
 import Divider from '@mui/material/Divider';
 import { ArrowForward } from '@mui/icons-material';
+import { Typography } from '@mui/material';
+import Grid from '@mui/material/Grid';
+import Container from '@mui/material/Container';
 import themeColorPink from '../../styles/theme';
 
 function DetailedInfo(props) {
@@ -18,87 +21,94 @@ function DetailedInfo(props) {
     props.propertyDetails;
 
   return (
-    <Wrapper>
-      <LeftColumn>
-        <div style={cssStyle}>
-          <p>
-            <InfoRow>
-              <Bold>Type: </Bold> {type}
-            </InfoRow>
-          </p>
-          <p>
-            <InfoRow>
-              <Bold>Style: </Bold> {style}
-            </InfoRow>
-          </p>
-          <p>
-            <InfoRow>
-              <Bold>Size: </Bold> {size}
-            </InfoRow>
-          </p>
-          <p>
-            <InfoRow>
-              <Bold>Lot Size: </Bold> {lotSize}
-            </InfoRow>
-          </p>
-          <p>
-            <InfoRow>
-              <Bold> House Age: </Bold> {age}
-            </InfoRow>
-          </p>
-          <p>
-            <InfoRow>
-              <Bold>Taxes: </Bold> {taxes}
-            </InfoRow>
-          </p>
-        </div>
-      </LeftColumn>
-      <RightColumn>
-        <div style={cssStyle}>
-          <p>
-            <InfoRow>
-              <Bold>Added: </Bold> {added}
-            </InfoRow>
-          </p>
-          <p>
-            <InfoRow>
-              <Bold>Updated: </Bold> {updated}
-            </InfoRow>
-          </p>
-          <p>
-            <InfoRow>
-              <Bold>Last Checked: </Bold> {lastChecked}
-            </InfoRow>
-          </p>
-          <p>
-            <InfoRow>
-              <Bold>MLS: </Bold> {mls}
-            </InfoRow>
-          </p>
-          <p>
-            <InfoRow>
-              <Bold>Source: </Bold> {source}
-            </InfoRow>
-          </p>
-          <p>
-            <InfoRow>
-              <Bold>Listed By: </Bold> {listedBy}
-            </InfoRow>
-          </p>
-        </div>
-        <Divider sx={{ borderBottomWidth: 5 }}></Divider>
-        <div style={cssStyle}>
-          <p>
-            <InfoRow>
-              <Bold>Description: </Bold> {description}
-            </InfoRow>
-          </p>
-        </div>
-      </RightColumn>
+    <Container maxWidth="xl" style={{ marginTop: '0rem', marginBottom: '7rem' }}>
+      <Grid container spacing={20}>
+        <Grid item xs={12} sm={6}>
+          <div style={{ textAlign: 'left', marginBottom: '2rem' }}>
+            <Typography variant="body1" sx={{ marginBottom: '1rem' }}>
+              <p>
+                <InfoRow>
+                  <Bold>Type: </Bold> {type}
+                </InfoRow>
+              </p>
+              <p>
+                <InfoRow>
+                  <Bold>Style: </Bold> {style}
+                </InfoRow>
+              </p>
+              <p>
+                <InfoRow>
+                  <Bold>Size: </Bold> {size}
+                </InfoRow>
+              </p>
+              <p>
+                <InfoRow>
+                  <Bold>Lot Size: </Bold> {lotSize}
+                </InfoRow>
+              </p>
+              <p>
+                <InfoRow>
+                  <Bold> House Age: </Bold> {age}
+                </InfoRow>
+              </p>
+              <p>
+                <InfoRow>
+                  <Bold>Taxes: </Bold> {taxes}
+                </InfoRow>
+              </p>
+            </Typography>
+          </div>
+        </Grid>
+        <Grid item xs={12} sm={6}>
+          <div style={{ textAlign: 'left', marginBottom: '2rem' }}>
+            <Typography variant="body1" sx={{ marginBottom: '1rem' }}>
+              <p>
+                <InfoRow>
+                  <Bold>Added: </Bold> {added}
+                </InfoRow>
+              </p>
+              <p>
+                <InfoRow>
+                  <Bold>Updated: </Bold> {updated}
+                </InfoRow>
+              </p>
+              <p>
+                <InfoRow>
+                  <Bold>Last Checked: </Bold> {lastChecked}
+                </InfoRow>
+              </p>
+              <p>
+                <InfoRow>
+                  <Bold>MLS: </Bold> {mls}
+                </InfoRow>
+              </p>
+              <p>
+                <InfoRow>
+                  <Bold>Source: </Bold> {source}
+                </InfoRow>
+              </p>
+              <p>
+                <InfoRow>
+                  <Bold>Listed By: </Bold> {listedBy}
+                </InfoRow>
+              </p>
+            </Typography>
+          </div>
+        </Grid>
+      </Grid>
+
+      <Divider sx={{ borderBottomWidth: 3 }}></Divider>
+      <div style={{ textAlign: 'center', marginBottom: '3rem', marginTop: '3rem' }}>
+        <p>
+          <InfoRow>
+            <Bold>Description: </Bold> {description}
+          </InfoRow>
+        </p>
+      </div>
       <Button variant="outlined" color="success" size="large" endIcon={<ArrowForward />}>
         See More Facts and Features
       </Button>
-    </Wrapper>
+    </Container>
   );
 }
 export default DetailedInfo;
@@ -120,16 +130,9 @@ const Section = styled.section`
 
 const Bold = styled.b`
   font-weight: bold;
+  margin-top: 30px;
 `;
 
 const Column = styled.div`
   flex: 1;
-`;
-
-const LeftColumn = styled(Column)`
-  order: 1;
-`;
-
-const RightColumn = styled(Column)`
-  order: 2;
 `;
