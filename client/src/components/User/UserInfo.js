db073b8dcf436cd03177df25eaadb7f0fda32e9c
Ji Young Kim <goldie.kim89@gmail.com>
2023-06-22 15:19:43 -0700

    feat: Format the phone number

--- client/src/components/User/UserInfo.js
+++ client/src/components/User/UserInfo.js
@@ -3,6 +3,13 @@ import { Link } from 'react-router-dom';
 import Button from '@mui/material/Button';
 import styled from 'styled-components';
 
+function formatPhoneNumber(phoneNumber) {
+  const areaCode = phoneNumber.slice(0, 3);
+  const firstPart = phoneNumber.slice(3, 6);
+  const secondPart = phoneNumber.slice(6);
+  return `(${areaCode}) ${firstPart}-${secondPart}`;
+}
+
 function UserInfo({ user }) {
   return (
     <Margin>
@@ -19,7 +26,7 @@ function UserInfo({ user }) {
         <Bold>Email: </Bold> {user.email}
       </p>
       <p>
-        <Bold>Phone Number: </Bold> {user.phoneNumber}
+        <Bold>Phone Number: </Bold> {formatPhoneNumber(user.phoneNumber)}
       </p>
       <p>
         <Bold>Region: </Bold> {user.region}
