a306f221d262f857d38ff772683d1e66a8b8ee0f
czha <czha@student.ubc.ca>
2023-06-22 06:44:09 -0400

    feat: added heart icon to PropertyCard

--- client/src/components/Property/PropertyCard.js
+++ client/src/components/Property/PropertyCard.js
@@ -1,19 +1,35 @@
 import './PropertyCard.css';
+import React, { useState } from 'react';
 import { useNavigate } from 'react-router-dom';
 import styled from 'styled-components';
+import FavoriteIcon from '@mui/icons-material/Favorite';
+import FavoriteBorderIcon from '@mui/icons-material/FavoriteBorder';
 import { convertPriceToCAD } from '../../utils/utils';
 
 function PropertyCard({ property }) {
   const navigate = useNavigate();
+  const [liked, setLiked] = useState(false);
+
   const navigateToPropertyPage = zpid => {
     navigate(`/properties/${zpid}`, {
       state: { zpid },
     });
   };
+
+  const handleLike = event => {
+    event.stopPropagation(); // prevent the click event from bubbling up to the parent
+    setLiked(!liked); // toggle the liked state
+  };
+
   return (
     <Img onClick={() => navigateToPropertyPage(property.zpid)}>
       <div className="property-card">
         <img src={property.imgSrc} alt="Property" className="property-image" />
+        {liked ? (
+          <StyledHeartLikedIcon onClick={handleLike} />
+        ) : (
+          <StyledHeartBorderIcon onClick={handleLike} />
+        )}
         <div className="property-info">
           <div className="property-price">
             {convertPriceToCAD(property.price)}
@@ -29,4 +45,18 @@ export default PropertyCard;
 
 const Img = styled.div`
   margin: 23px;
+`;
+
+const StyledHeartBorderIcon = styled(FavoriteBorderIcon)`
+  position: absolute;
+  top: 10px;
+  right: 10px;
+  color: white;
+`;
+
+const StyledHeartLikedIcon = styled(FavoriteIcon)`
+  position: absolute;
+  top: 10px;
+  right: 10px;
+  color: red;
 `;
--- client/src/pages/Likes/Likes.js
+++ client/src/pages/Likes/Likes.js
@@ -1,20 +1,45 @@
 import React, { useEffect } from 'react';
 import { useLocation, useNavigate } from 'react-router-dom';
 import styled from 'styled-components';
+import { useDispatch, useSelector } from 'react-redux';
+import PropertyGrid from '../../components/Property/PropertyGrid';
+import demoHouseImage from '../../assets/images/demoHouse.jpg';
+import { getListAsync } from '../../redux/home/thunks';
+import SearchComponent from '../../components/SearchOption/SearchComponent';
 
-function Likes() {
+function Home() {
   const navigate = useNavigate();
   const location = useLocation();
 
-  return <Wrapper />;
-}
-export default Likes;
+  const properties = useSelector(state => state.home.list);
 
-const Wrapper = styled.div`
-  padding-top: 6em;
-  width: 100vw;
-  height: 100vh;
+  const dispatch = useDispatch();
+  useEffect(() => {
+    dispatch(getListAsync());
+  }, [dispatch]);
+
+  return (
+    <Margin>
+      <Main>
+        <p>Liked Properties</p>
+        <PropertyGrid properties={properties} />
+      </Main>
+    </Margin>
+  );
+}
+
+const Main = styled.div`
+  padding-top: 5em;
+  width: 100%;
   display: flex;
   flex-direction: column;
-  align-item: space-around;
 `;
+
+const Margin = styled.div`
+  margin: 20px;
+  font-family: arial, sans-serif;
+  line-height: 30pt;
+  text-align: center;
+`;
+
+export default Home;
