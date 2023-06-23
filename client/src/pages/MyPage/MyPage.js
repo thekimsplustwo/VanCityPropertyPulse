df1975e1396c7cb2fcc452f72c55ff949ba9f301
Ji Young Kim <goldie.kim89@gmail.com>
2023-06-22 20:30:55 -0700

    chore: Removed an unnecessary const

--- client/src/pages/MyPage/MyPage.js
+++ client/src/pages/MyPage/MyPage.js
@@ -30,14 +30,6 @@ function MyPage() {
     dispatch(getUserAsync(USER_EMAIL));
   }, [dispatch]);
 
-  const Item = styled(Paper)(({ theme }) => ({
-    backgroundColor: theme.palette.mode === 'dark' ? '#1A2027' : '#fff',
-    ...theme.typography.body2,
-    padding: theme.spacing(1),
-    textAlign: 'center',
-    color: theme.palette.text.secondary,
-  }));
-
   return (
     <Main>
       <Box
