e58e7994f4d5b3ce98bdcde491d142bfa041daf8
Chohyeon kim <chohyeonkim94@gmail.com>
2023-06-29 18:16:22 -0700

    feat: add searchReducer on store.js

--- client/src/redux/store.js
+++ client/src/redux/store.js
@@ -3,10 +3,12 @@ import usersReducer from './users/reducer';
 import homeReducer from './home/reducer';
 import propertyReducer from './property/reducer';
 import likesReducer from './likes/reducer';
+import searchReducer from './search/reducer';
 
 export const store = configureStore({
   reducer: {
     users: usersReducer,
+    search: searchReducer,
     home: homeReducer,
     property: propertyReducer,
     likes: likesReducer,
