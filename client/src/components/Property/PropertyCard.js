756d668a9d7bedb605797dc7f481a153f61c278d
Chohyeon kim <chohyeonkim94@gmail.com>
2023-07-07 11:30:15 -0700

    feat add Compare btn and nvigate to compare page

--- client/src/components/Property/PropertyCard.js
+++ client/src/components/Property/PropertyCard.js
@@ -6,6 +6,7 @@ import { useNavigate } from 'react-router-dom';
 import styled from 'styled-components';
 import FavoriteIcon from '@mui/icons-material/Favorite';
 import FavoriteBorderIcon from '@mui/icons-material/FavoriteBorder';
+import ShowChartIcon from '@mui/icons-material/ShowChart';
 import { convertPriceToCAD } from '../../utils/utils';
 
 import { addLikesAsync, deleteLikesAsync } from '../../redux/likes/thunks';
@@ -22,6 +23,11 @@ function PropertyCard({ property }) {
     navigate(`/properties/${zpid}`, {
       state: { zpid },
     });
+  };
+
+  const handleCompare = event => {
+    event.stopPropagation();
+    navigate('/compare', { state: { property } });
   };
 
   const handleAddLike = event => {
@@ -47,11 +53,22 @@ function PropertyCard({ property }) {
         ) : (
           <StyledHeartBorderIcon onClick={handleAddLike} />
         )}
+        <StatusWrapper>
+          <StyledStatusIcon />
+          <StatusText>{property.listingStatus}</StatusText>
+        </StatusWrapper>
+        <CompareButton onClick={handleCompare}>Compare</CompareButton>
+
         <div className="property-info">
           <div className="property-price">
             {convertPriceToCAD(property.price)}
           </div>
-          <div className="property-location">{property.address}</div>
+          <div>
+            <span className="property-bed">Bed {property.bedrooms}, </span>
+            <span className="property-bath">Bed {property.bathrooms}, </span>
+            <span className="property-area">{property.livingArea} sqft, </span>
+          </div>
+          <PropertyLocation>{property.address}</PropertyLocation>
         </div>
       </div>
     </Img>
@@ -76,4 +93,53 @@ const StyledHeartLikedIcon = styled(FavoriteIcon)`
   top: 10px;
   right: 10px;
   color: red;
+`;
+
+const StyledStatusIcon = styled(ShowChartIcon)`
+  display: flex;
+  align-items: center;
+  color: white;
+  font-size: small;
+`;
+
+const StatusWrapper = styled.div`
+  position: absolute;
+  top: 10px;
+  left: 10px;
+  display: flex;
+  align-items: center;
+  background-color: pink;
+  border-radius: 10px;
+  padding: 2px;
+  height: 1.5rem;
+`;
+
+const StatusText = styled.span`
+  margin-left: 5px;
+  margin-right: 2px;
+  color: white;
+  font-size: 0.85rem;
+  font-weight: bold;
+`;
+
+const PropertyLocation = styled.div`
+  font-size: 1rem;
+`;
+
+const CompareButton = styled.button`
+  position: absolute;
+  bottom: 105px;
+  right: 5px;
+  background-color: rgba(255, 255, 255, 0.8);
+  border: none;
+  color: black;
+  padding: 5px 10px;
+  text-align: center;
+  text-decoration: none;
+  display: inline-block;
+  font-size: 15px;
+  margin: 4px 2px;
+  cursor: pointer;
+  border-radius: 10px;
+  font-weight: bold;
 `;
