d82fb9a9c902619c7fa913d41ad438df1ce9e428
Ji Young Kim <goldie.kim89@gmail.com>
2023-07-13 11:52:49 -0700

    feat: no user is logged in when the server starts

--- server/src/server.js
+++ server/src/server.js
@@ -6,6 +6,7 @@ import logger from 'morgan';
 import passport from 'passport';
 import session from 'express-session';
 import connectMongoDBSession from 'connect-mongodb-session';
+import cookieParser from 'cookie-parser';
 import connect from './schemas/index.js';
 import routes from './routes/index.js';
 import passportIndex from './middleware/index.js';
@@ -18,22 +19,24 @@ const { MONGODB_HOST, MONGODB_USER, MONGODB_PW, DATABASE, SECRET_KEY } =
 const PORT = process.env.PORT || 10010;
 
 const MongoDBStore = connectMongoDBSession(session);
+const sessionStore = new MongoDBStore({
+  uri: `mongodb+srv://${MONGODB_USER}:${MONGODB_PW}@${MONGODB_HOST}/${DATABASE}?retryWrites=true&w=majority`,
+  collection: 'sessions',
+});
 
 connect();
 
 const app = express();
 app.use(express.json());
 app.use(logger('dev'));
+app.use(cookieParser());
 
 app.use(
   session({
     secret: `${SECRET_KEY}`,
     resave: false,
     saveUninitialized: false,
-    store: new MongoDBStore({
-      uri: `mongodb+srv://${MONGODB_USER}:${MONGODB_PW}@${MONGODB_HOST}/${DATABASE}?retryWrites=true&w=majority`,
-      collection: 'sessions',
-    }),
+    store: sessionStore,
   })
 );
 app.use(passport.initialize());
@@ -59,6 +62,21 @@ const start = async () => {
     server.listen(PORT, () =>
       console.log(`Server is listening on ${PORT} | MOCK ${process.env.MOCK}`)
     );
+
+    try {
+      await new Promise((resolve, reject) => {
+        sessionStore.clear(err => {
+          if (err) {
+            console.error('Error destroying sessions:', err);
+            reject(err);
+          } else {
+            resolve();
+          }
+        });
+      });
+    } catch (err) {
+      console.error('Error during server shutdown:', err);
+    }
   } catch (err) {
     console.error(err);
   }
