b2f44347ce1c96a65929bf63b3c2e0e587c65663
Chohyoen Kim (Elly) <104462860+chohyeonkim@users.noreply.github.com>
2023-07-19 00:29:07 -0700

    fix: Property img (#186)

--- client/src/components/Property/PropertyImage.js
+++ client/src/components/Property/PropertyImage.js
@@ -1,4 +1,5 @@
 import React, { useState } from 'react';
+import styled from 'styled-components';
 import placeholderImage from '../../assets/images/imgNotFound.png';
 
 function PropertyImage({ src, alt }) {
@@ -8,7 +9,13 @@ function PropertyImage({ src, alt }) {
     setImgSrc(placeholderImage);
   };
 
-  return <img src={imgSrc} alt={alt} onError={handleError} />;
+  return <NewPropertyImage src={imgSrc} alt={alt} onError={handleError} />;
 }
+
+const NewPropertyImage = styled.img`
+  width: 100%;
+  height: 100%;
+  object-fit: cover;
+`;
 
 export default PropertyImage;
