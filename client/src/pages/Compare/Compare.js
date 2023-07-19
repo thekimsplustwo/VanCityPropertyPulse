f191c9c5309b2c6ac14323efc213dd79b2d2d6d7
czha <czha@student.ubc.ca>
2023-07-19 16:28:03 -0700

    ops: clean up duplicate zpid in paras to show only once

--- client/src/pages/Compare/Compare.js
+++ client/src/pages/Compare/Compare.js
@@ -19,22 +19,32 @@ import { getPropertyAsync } from '../../redux/property/thunks';
 function Compare() {
   const [isModalOpen, setIsModalOpen] = useState(false);
   const params = new URL(document.location).searchParams;
-  let zpidList = params.getAll('item');
-  const navigate = useNavigate(); // add this line
+  let zpidList = Array.from(new Set(params.getAll('item')));
+  const navigate = useNavigate();
+
+  zpidList = zpidList.slice(-3); // Get the last three items always
 
   if (zpidList.length > 3) {
     alert(
       'Only 3 properties can be compared at a time. Showing the last three properties.'
     );
-    zpidList = zpidList.slice(-3); // Get the last three items
   }
 
-  const [propertyList, setPropertyList] = useState(new Array(3).fill(null));
-
+  const [propertyList, setPropertyList] = useState(
+    new Array(zpidList.length).fill(null)
+  );
   const dispatch = useDispatch();
   useEffect(() => {
     zpidList.forEach((zpid, index) => {
-      dispatch(getPropertyAsync(zpid)).then(property => {
+      dispatch(getPropertyAsync(zpid)).then(response => {
+        // Make a copy of the property
+        const property = { ...response.payload };
+
+        // Ensure property.imgSrc is always an array
+        property.imgSrc = Array.isArray(property.imgSrc)
+          ? property.imgSrc
+          : [property.imgSrc];
+
         setPropertyList(prevPropertyList => {
           const newPropertyList = [...prevPropertyList];
           newPropertyList[index] = property;
@@ -43,17 +53,19 @@ function Compare() {
       });
     });
   }, [dispatch]);
+
   const handleOpenModal = () => {
     setIsModalOpen(true);
   };
   const handleCloseModal = () => {
     setIsModalOpen(false);
   };
-  // add this function
+
   const handleClear = () => {
     navigate('/compare', { replace: true });
     window.location.reload();
   };
+
   return (
     <Wrapper>
       <Margin>
@@ -86,13 +98,15 @@ function Compare() {
             <Grid container spacing={2}>
               {propertyList.map((property, index) => (
                 <Grid
-                  item="true"
-                  xs={4}
+                  item
+                  xs={12}
+                  sm={6}
+                  md={4}
                   key={property ? property.zpid : `property-${index}`}
                 >
                   {property && (
                     <>
-                      <ImageCarousel propertyImages={property.imgSrc} />
+                      <ImageCarousel propertyImages={property?.imgSrc || []} />
                       <CompareProps propertyDetails={property} />
                     </>
                   )}
@@ -109,7 +123,6 @@ const Main = styled.div`
   padding-top: 1em;
   min-height: 100vh;
   width: 100vw;
-  height: 100vh;
   display: flex;
   flex-direction: column;
   justify-content: center;
