d8663ba8cb85e4e78c2d79d30615401d61329c53
jieunkxx <jieun.k@hotmail.com>
2023-07-18 18:30:53 -0700

    feat: add redux persist

--- client/src/index.js
+++ client/src/index.js
@@ -2,21 +2,25 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { GoogleOAuthProvider } from '@react-oauth/google';
 import { Provider } from 'react-redux';
-import { configureStore } from '@reduxjs/toolkit';
-import { store } from './redux/store';
+import { PersistGate } from 'redux-persist/integration/react';
 import './index.css';
+import { getStore } from './redux/store';
 import App from './App';
 import GlobalStyle from './global';
 import { GOOGLE_CLIENT_ID } from './config';
 
+const { persistor, store } = getStore();
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <GoogleOAuthProvider clientId={GOOGLE_CLIENT_ID}>
-    <React.StrictMode>
-      <Provider store={store}>
-        <GlobalStyle />
-        <App />
-      </Provider>
-    </React.StrictMode>
+    <Provider store={store}>
+      <PersistGate loading={null} persistor={persistor}>
+        <React.StrictMode>
+          <GlobalStyle />
+          <App />
+        </React.StrictMode>
+      </PersistGate>
+    </Provider>
   </GoogleOAuthProvider>
 );
--- client/src/redux/store.js
+++ client/src/redux/store.js
@@ -1,17 +1,34 @@
-import { configureStore } from '@reduxjs/toolkit';
+import { createStore, applyMiddleware } from 'redux';
+import { combineReducers, configureStore } from '@reduxjs/toolkit';
+import { persistReducer, persistStore } from 'redux-persist';
+import storage from 'redux-persist/lib/storage';
+import thunk from 'redux-thunk';
 import usersReducer from './users/reducer';
 import homeReducer from './home/reducer';
 import propertyReducer from './property/reducer';
 import likesReducer from './likes/reducer';
 import searchReducer from './search/reducer';
 
-export const store = configureStore({
-  reducer: {
-    users: usersReducer,
-    search: searchReducer,
-    home: homeReducer,
-    property: propertyReducer,
-    likes: likesReducer,
-  },
-  devTools: true,
+const persistConfig = {
+  key: 'root', // localStorage key
+  storage, // localStorage
+  whitelist: ['users', 'home', 'likes', 'property'], // target (reducer name)
+};
+
+const rootReducer = combineReducers({
+  users: usersReducer,
+  search: searchReducer,
+  home: homeReducer,
+  property: propertyReducer,
+  likes: likesReducer,
 });
+
+export const persistedReducer = persistReducer(persistConfig, rootReducer);
+export const getStore = () => {
+  const store = configureStore({
+    reducer: persistedReducer, // used persistedReducer here
+    middleware: [thunk],
+  });
+  const persistor = persistStore(store);
+  return { store, persistor };
+};
