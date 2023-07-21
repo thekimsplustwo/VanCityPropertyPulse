eec687cd4349536f499a8f81787684a2cde10f80
Ji Young Kim <goldie.kim89@gmail.com>
2023-07-20 17:42:45 -0700

    feat: place a button to navigate to prev page

--- client/src/components/Property/PropertyTitle.js
+++ client/src/components/Property/PropertyTitle.js
@@ -1,4 +1,7 @@
 import styled from 'styled-components';
+import { Tooltip } from '@mui/material';
+import ArrowCircleLeftOutlinedIcon from '@mui/icons-material/ArrowCircleLeftOutlined';
+import { useNavigate } from 'react-router-dom';
 import { convertPriceToCAD } from '../../utils/utils';
 
 function PropertyHeader({ propertyDetails }) {
@@ -9,15 +12,30 @@ function PropertyHeader({ propertyDetails }) {
     streetAddress: propertyDetails.address?.streetAddress ?? '',
     zipcode: propertyDetails.address?.zipcode ?? '',
   };
+
+  const navigate = useNavigate();
+
+  const handleGoBack = () => {
+    navigate(-1);
+  };
+
   return (
     <Wrapper>
       <HeaderWrapper>
-        <HeaderTitle>
-          <h1>{address.streetAddress}</h1>
-          <h3>
-            {address.city}, {address.state}
-          </h3>
-        </HeaderTitle>
+        <HeaderTitleWrapper>
+          <Tooltip title="Previous Page">
+            <ArrowCircleLeftOutlinedIcon
+              onClick={handleGoBack}
+              style={{ fontSize: '60px', cursor: 'pointer' }}
+            />
+          </Tooltip>
+          <HeaderTitle>
+            <h1>{address.streetAddress}</h1>
+            <h3>
+              {address.city}, {address.state}
+            </h3>
+          </HeaderTitle>
+        </HeaderTitleWrapper>
         <HeaderSubTitle>
           <h1>{convertPriceToCAD(propertyDetails.price)}</h1>
           <h3>
@@ -43,6 +61,14 @@ const HeaderWrapper = styled.div`
   justify-content: space-between;
   align-items: stretch;
   padding: 20px 0;
+`;
+
+const HeaderTitleWrapper = styled.div`
+  display: flex;
+  justify-content: space-between;
+  align-items: flex-start;
+  flex-direction: row;
+  gap: 10px;
 `;
 
 const HeaderTitle = styled.div`
