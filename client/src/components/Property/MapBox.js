104e477028f46302e10cac4267cebcb8f7fff0f5
czha <czha@student.ubc.ca>
2023-07-22 12:55:05 -0400

    feat: add MapBox in Detailed Property

---
+++ client/src/components/Property/MapBox.js
@@ -0,0 +1,42 @@
+// MapBox.js
+import React, { useEffect, useRef } from 'react';
+import mapboxgl from 'mapbox-gl';
+
+mapboxgl.accessToken = process.env.REACT_APP_MAPBOX_TOKEN;
+
+function MapBox({ longitude, latitude }) {
+  const mapContainer = useRef(null);
+  console.log('longitude', longitude);
+  console.log('latitude', latitude);
+
+  useEffect(() => {
+    const map = new mapboxgl.Map({
+      container: mapContainer.current,
+      style: 'mapbox://styles/mapbox/streets-v11',
+      center: [longitude, latitude],
+      zoom: 13,
+    });
+
+    const marker = new mapboxgl.Marker()
+      .setLngLat([longitude, latitude])
+      .addTo(map);
+
+    return () => {
+      map.remove();
+      marker.remove();
+    };
+  }, [longitude, latitude]);
+
+  return (
+    <div
+      ref={mapContainer}
+      style={{
+        width: '83%',
+        height: '50%',
+        position: 'relative',
+      }}
+    />
+  );
+}
+
+export default MapBox;
