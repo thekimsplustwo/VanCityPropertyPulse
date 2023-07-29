229cd2ce3e8a68bf26587fd90f603b4e25ec6fb4
jieunkxx <jieun.k@hotmail.com>
2023-07-29 11:10:30 -0700

    fix: getUser controller

--- server/src/controller/user.js
+++ server/src/controller/user.js
@@ -5,12 +5,12 @@ import { ERROR_TYPE, errorGenerator } from '../utils/error.js';
 dotenv.config();
 
 const getUserInfoByEmail = async (req, res) => {
-  const { email, source } = req.user;
-  if (!email) {
-    errorGenerator(ERROR_TYPE.INVALID_REQUEST);
-  }
-  const user = await userService.getUserInfoByEmail(email, source);
-  return res.status(200).json(user);
+  //const { email, source } = req.user;
+  // if (!email) {
+  //   errorGenerator(ERROR_TYPE.INVALID_REQUEST);
+  // }
+  // const user = await userService.getUserInfoByEmail(email, source);
+  return res.status(200).json(req.user);
 };
 
 const updateUserInfo = async (req, res) => {
