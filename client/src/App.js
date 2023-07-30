b847e54526308a46d56c63537f5f44a65be4a944
jieunkxx <jieun.k@hotmail.com>
2023-07-30 11:24:02 -0700

    deploy: hashRouter + hashed endpoint

--- client/src/App.js
+++ client/src/App.js
@@ -1,8 +1,13 @@
 import './App.css';
+import { HashRouter, Route } from 'react-router-dom';
 import Router from './Router';
 
 function App() {
-  return <Router />;
+  return (
+    <HashRouter>
+      <Router />;
+    </HashRouter>
+  );
 }
 
 export default App;
--- server/src/routes/index.js
+++ server/src/routes/index.js
@@ -8,11 +8,11 @@ import authRouter from './auth.js';
 
 const router = express.Router();
 
-router.use('/home', homeRouter);
-router.use('/properties', propertyRouter);
-router.use('/users', userRouter);
-router.use('/likes', likesRouter);
-router.use('/compare', compareRouter);
-router.use('/auth', authRouter);
+router.use('/#/home', homeRouter);
+router.use('/#/properties', propertyRouter);
+router.use('/#/users', userRouter);
+router.use('/#/likes', likesRouter);
+router.use('/#/compare', compareRouter);
+router.use('/#/auth', authRouter);
 
 export default router;
