802a651d38702fc3cae1a3e4c07c9c0c5b01dcae
jieunkxx <jieun.k@hotmail.com>
2023-08-01 22:32:54 -0700

    fix: mapbox rendering on combined server

--- client/src/components/Property/MapBox.js
+++ client/src/components/Property/MapBox.js
@@ -3,7 +3,10 @@ import { useState, useEffect, useRef } from 'react';
 import mapboxgl from 'mapbox-gl';
 import { MAPBOX_STYLE, MAPBOX_TOKEN, MAPBOX_ZOOM } from '../../config';
 import 'mapbox-gl/dist/mapbox-gl.css';
+// eslint-disable-next-line import/no-webpack-loader-syntax, import/order, import/no-unresolved
+import mapboxCspWorker from 'worker-loader!mapbox-gl/dist/mapbox-gl-csp-worker';
 
+mapboxgl.workerClass = mapboxCspWorker;
 mapboxgl.accessToken = MAPBOX_TOKEN;
 
 function MapBox({ longitude, latitude }) {
