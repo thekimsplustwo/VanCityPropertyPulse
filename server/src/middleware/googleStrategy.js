f0ce57103cde4c5c31c42d49b28fd91cb8c76810
jieunkxx <jieun.k@hotmail.com>
2023-08-01 21:49:58 -0700

    refactor

--- server/src/middleware/googleStrategy.js
+++ server/src/middleware/googleStrategy.js
@@ -6,10 +6,12 @@ import generateToken from '../utils/token.js';
 
 dotenv.config();
 
+const { GOOGLE_CLIENT_ID, GOOGLE_CLIENT_SECRET, SERVER_HOST } = process.env;
+
 const GOOGLE_OAUTH_OPTION = {
-  clientID: process.env.GOOGLE_CLIENT_ID,
-  clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-  callbackURL: `${process.env.SERVER_HOST}/auth/google/callback`,
+  clientID: GOOGLE_CLIENT_ID,
+  clientSecret: GOOGLE_CLIENT_SECRET,
+  callbackURL: `${SERVER_HOST}/auth/google/callback`,
   scope: ['email', 'profile'],
   passReqToCallback: true,
 };
