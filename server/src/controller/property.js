c22cf228e3527afa4e83019d8ccb15093ddad7a0
chris <chris019@student.ubc.ca>
2023-08-01 21:51:28 -0700

    ops: revert compare page rendering
    This reverts commit f8c7e44f725b9d1a4ac745f1830b2d9e9f06a01c.

--- server/src/controller/property.js
+++ server/src/controller/property.js
@@ -4,43 +4,13 @@ import { ERROR_TYPE, errorGenerator } from '../utils/error.js';
 
 dotenv.config();
 
-let lastRequestTime = 0;
-const INTERVAL_MS = 600;
-
-const sleep = ms => {
-  return new Promise(resolve => {
-    setTimeout(() => {
-      resolve();
-    }, ms);
-  });
-};
-
-const delayNextRequest = async () => {
-  const currentTime = Date.now();
-  const timeSinceLastRequest = currentTime - lastRequestTime;
-  const delay = Math.max(INTERVAL_MS - timeSinceLastRequest, 0);
-  lastRequestTime = currentTime + delay;
-
-  return sleep(delay);
-};
-
 const getPropertyDetails = async (req, res) => {
-  try {
-    const { zpid } = req.params;
-    if (!zpid) {
-      errorGenerator(ERROR_TYPE.INVALID_REQUEST);
-    }
-
-    // Enforce the delay between requests
-    await delayNextRequest();
-
-    const propertyDetails = await propertyService.getPropertyDetails(zpid);
-    return res.status(200).json(propertyDetails);
-  } catch (error) {
-    // Handle the error appropriately here
-    console.error(error);
-    return res.status(500).json({ error: 'An error occurred' });
+  const { zpid } = req.params;
+  if (!zpid) {
+    errorGenerator(ERROR_TYPE.INVALID_REQUEST);
   }
+  const propertyDetails = await propertyService.getPropertyDetails(zpid);
+  return res.status(200).json(propertyDetails);
 };
 
 const getPropertyWalkScore = async (req, res) => {
