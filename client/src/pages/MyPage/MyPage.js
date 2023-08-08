6e102f120b89ae85fc8d7d24dd74830ab237f755
Chohyoen Kim (Elly) <104462860+chohyeonkim@users.noreply.github.com>
2023-08-08 09:53:06 -0700

    fix: when no region data footer comes up (#327)

--- client/src/pages/MyPage/MyPage.js
+++ client/src/pages/MyPage/MyPage.js
@@ -35,7 +35,7 @@ function MyPage() {
     isLogin && (
       <Main>
         {modal && <UserProfileEdit setModal={setModal} />}
-        <StyledBox>
+        <StyledBox hasRegion={!!region}>
           <Grid container spacing={4}>
             <Grid item xs={12} sm={4}>
               <UserPageLeft setModal={setModal} />
@@ -64,6 +64,7 @@ const Main = styled.div`
 const StyledBox = styled(Box)`
   width: 100%;
   padding-top: 5em;
+  min-height: ${props => (props.hasRegion ? 'auto' : '100vh')};
 
   @media (max-width: 600px) {
     height: 100%;
